d5528d1aeeae33c9c649f7dd0bf31c9c03560f13
jprid <mackattack.bg@gmail.com>
2024-12-27 15:00:39 -0600

    use simplified version of magnitude calculation (skip squaring) for sobel

--- run.py
+++ run.py
@@ -39,7 +39,7 @@ class ImageProcessor(QThread):
         sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
         
         # Calculate gradient magnitude
-        magnitude = np.sqrt(sobelx**2 + sobely**2)
+        magnitude = abs(sobelx) + abs(sobely)
         # Normalize to 0-255
         magnitude = np.uint8(255 * magnitude / np.max(magnitude))
         
